5f0ccab0ef05e1e70307a22351009a8b66661fc7
Nikhil <npathania502@gmail.com>
2024-10-26 15:11:45 +0530

    server.js changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import User from './models/User.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(()=>console.log("Connected to MongoDB"))
+.catch((error)=>console.log("MongoDB connection error: ",error));
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +24,18 @@ app.get("/api/message",(req,res) => {
 app.get("/api/info",(req,res) => {
     res.json({message:"User name is Nik"});
 });
+
+app.post("/api/submit",async(req,res) => {
+    try{
+        const {name,email,age} = req.body;
+        const newUser = new User({name,email,age});
+        await newUser.save();
+        res.status(200).json({message:"User data saved successfully",user:newUser});
+    }
+    catch(error){
+        res.status(500).json({message:"Internal server error",error});
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
